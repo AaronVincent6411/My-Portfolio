216cd2cdfd2a2b30d7f38aa2f996e9df7b37e7f4
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2022-02-19 12:11:37 -0500

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,126 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I am Aaron P Laju.I am a second year B.Tech student currently studying in Adi Shankara Institute of Engineering and Technology Kalady.",
+				"I am a developer currently learning in Computer Vision stack and I am good in doing poster designing",
+				"I am a peron with good sportsman spirit and good mentality.",
+			],
+		},
+		contact: {
+			text: [
+				"You can connect me any time through Gmail or Discord. My mail ID:aaronvincent6411@gmail.com", 
+				"Discord: Aaron Vincent Aaron Laju#0922",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "C++,Python,C,Java",
+			color: "1",
+		},
+		{
+			title: "Clouds",
+			skillName: "Google Cloud Console",
+			color: "3",
+		},
+		{
+			title: "Poster Design Tools",
+			skillName: "Canva,GIMP",
+			color: "4",
+		},
+		{
+			title: "Communication Skill",
+			skillName: "Friendly, Confident, Respect, Leadership",
+			color: "5",
+		},
+		{
+			title: "IDEs using",
+			skillName: "VS Code, Pycharm",
+			color: "6",
+		},
+		{
+			title: "Linux",
+			skillName: "Ubuntu,Fedora",
+			color: "7",
+		},
+	],
+	projects: {
+		Projects: [
+			{
+				projectName: "Litter Detection",
+				image: "https://drive.google.com/file/d/1fCbvyz2bjLaXkdnx4uFhLVC3xHfEjw35/view?usp=sharing",
+				summary:
+					"Litter Detection Sytsem which would detect the litters and the information is passed to respective authorities.",
+				preview: "https://github.com/AaronVincent6411/Litter_Detection",
+				techStack: ["Python"],
+			},
+			{
+				projectName: "Vaccination",
+				summary:
+					"Developed a Database for collecting information of people who took vaccine",
+				preview: "https://github.com/AaronVincent6411/Vaccination",
+				techStack: ["C"],
+			},
+			{
+				projectName: "SmartC",
+				image: "https://drive.google.com/file/d/1GnDBD2oKLKhMYAAaZeKiysim8yEH0Ai2/view?usp=sharing",
+				summary:
+					"SmartC is a platform for educational institutions for accessing and providing effective way of management to students and teachers of a campus",
+				preview: "https://github.com/AaronVincent6411/SmartC",
+				techStack: ["HTML", "Python"],
+			},
+		],
+	},
+	education: [
+		{
+			title: "Bachelors in Computer Science and Engineering",
+			duration: "2020 - Present",
+			subtitle: "Adi Shankara Institute Of Engineering and Technology,Kalady",
+			details: [
+				"Course under taking is B.Tech Computer Science(Artificial Intelligence)",
+				"Currently Second year student",
+				"Execom member of Computer Society of India",
+				"HackClub Volunteer",
+				"National Service Scheme(NSS) Volunteer",
+				"TinkerHub Member",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"OOPS Java",
+				"C Programming",
+			],
+		},
+		{
+			title: "Class 11-12th in Computer Science and Mathematics",
+			duration: "2018-2020",
+			subtitle: "Central Board of Secondary Education",
+			details: [
+				"Secured 85.6 percentile in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics", "Computer Science"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/aaronvincentaaronlaju6411",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/aaronvincent6411",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/6411Aaron?s=08",
+				},
+			],
+		},
+	],
+};
