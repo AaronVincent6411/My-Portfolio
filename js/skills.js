8ddf446102fe4d704bd4ecb4ee916e56351c24a0
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2022-02-20 03:25:48 -0500

    Add files via upload

---
+++ js/skills.js
@@ -0,0 +1,34 @@
+export default {
+	skills: [
+		{
+			title: "Languages",
+			skillName: "C++,Python,C,Java",
+			color: "1",
+		},
+		{
+			title: "Clouds",
+			skillName: "Google Cloud Console",
+			color: "3",
+		},
+		{
+			title: "Poster Design Tools",
+			skillName: "Canva,GIMP",
+			color: "4",
+		},
+		{
+			title: "Communication Skill",
+			skillName: "Friendly, Confident, Respect, Leadership",
+			color: "5",
+		},
+		{
+			title: "IDEs using",
+			skillName: "VS Code, Pycharm",
+			color: "6",
+		},
+		{
+			title: "Linux",
+			skillName: "Ubuntu,Fedora",
+			color: "7",
+		},
+	],
+};
