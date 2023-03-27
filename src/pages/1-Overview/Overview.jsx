1f01bc92b0a0954be90042663b4c994c70f637a6
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-03-27 15:35:46 -0400

    modified inner rectangle to a navbar

--- src/pages/1-Overview/Overview.jsx
+++ src/pages/1-Overview/Overview.jsx
@@ -1,12 +1,37 @@
 import './Overview.css';
+import Typed from "typed.js";
 
 function Overview () {
+//   useEffect(() => {
+//     const typed = new Typed(el.current, {
+//         strings: ["Website is currently under maintenance"], 
+//         startDelay: 200,
+//         typeSpeed: 100,
+//         backSpeed: 100,
+//         backDelay: 100,
+//         loop: false,
+//     });
+//     return () => {
+//         typed.destroy();
+//     };
+// }, []);
+// const el = useRef(null);
 
 return(
     <div className="container">
       <div className="rectangle">
-        <div className='inner-rectangle'></div>
-        <h1 className='heading'>Website is currently under maintenance</h1>
+        <div className='inner-rectangle'>
+          <nav>
+            <ul>
+            <li><a href="#">Overview</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Timeline</a></li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className='heading'>Website is currently under maintenance</div>
       </div>
     </div>
   )
