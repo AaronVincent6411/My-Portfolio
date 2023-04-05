967b5076bf02be7df7d84721965ac3e1767b10b8
= <=>
2023-04-05 02:38:17 -0400

    defined sections in pages for export

---
+++ src/pages/pages.jsx
@@ -0,0 +1,5 @@
+export { Home } from "./1-Overview/Overview.jsx";
+export { About} from "./2-About/About.jsx";
+export { Projects } from "./3-Projects/Projects.jsx";
+export { Timeline } from "./4-Timeline/Timeline.jsx";
+export { Contact } from "./5-Contact/Contact.jsx";
