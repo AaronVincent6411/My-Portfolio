c1461acb55dca484462ce0909df450b1510a07ab
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-04-07 03:52:16 -0400

    changed project icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FaClock } from 'react-icons/fa';
-import { AiOutlineMail } from 'react-icons/ai';
+import { AiOutlineMail, AiFillProject } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import projectIcon from '../../assets/Projects-icon.png'
+
+// import { IconName } from "react-icons/md";
+import { GrProjects } from "react-icons/gr";
 
 function Navbar() {
 
@@ -24,11 +28,7 @@ return(
           </Link>
         </li>
         <li>
-          <Link to="/projects">
-            <div className="image">
-              <img src='/src/assets/Projects-icon.png'/>&nbsp; &nbsp;Projects
-            </div>
-          </Link>
+          <Link to="/projects"><AiFillProject />&nbsp; &nbsp;Projects</Link>
         </li>
         <li>
           <Link to="/timeline"><FaClock />&nbsp; &nbsp; Timeline</Link>
