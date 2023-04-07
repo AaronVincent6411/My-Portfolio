0f2dfa0d299881916269f4880e31231f6df431cf
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-04-07 03:55:35 -0400

    added a small rectangle

--- src/pages/3-Projects/Projects.jsx
+++ src/pages/3-Projects/Projects.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+// import React from "react";
+// import Navbar from "../../components/Navbar/Navbar";
+// import Header from "../../components/Header/HeaderP";
+// import './Projects.css'
+
+// function Projects() {
+//     const myStyle = {
+//         background: 'linear-gradient(to right, #632636, #392A48)',
+//         height: '100vh',
+//         width: '100vw'
+//       };
+
+//     return(
+//         <div style={myStyle}>
+//             <div className="container">
+//                 <div className="rectangle">
+//                    <Navbar/>
+//                    <Header/>
+//                    <div className='heading'>Projects</div>
+//                    <div className="project-rectangle">
+//                    </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Projects
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/HeaderP";
+import "./Projects.css";
 
 function Projects() {
-    const myStyle = {
-        background: 'linear-gradient(to right, #632636, #392A48)',
-        height: '100vh',
-        width: '100vw'
-      };
+  const myStyle = {
+    background: "linear-gradient(to right, #632636, #392A48)",
+    height: "100vh",
+    width: "100vw",
+  };
 
-    return(
-        <div style={myStyle}>
-            <div className="container">
-                <div className="rectangle">
-                   <Navbar/>
-                   <Header/>
-                   <div className='heading'>Projects</div>
-                </div>
-            </div>
+  return (
+    <div style={myStyle}>
+      <div className="container">
+        <div className="rectangle">
+          <Navbar />
+          <Header />
+          <div className="heading">Projects</div>
+          <div className="project-rectangle">
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
