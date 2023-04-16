044c0cd206c4d1c6904cf58e1ce6b1adf634fa1e
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-04-16 14:13:26 -0400

    created achivements

---
+++ src/components/Achievements/Achievements.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Achievements.css'
+
+function Achievements() {
+
+    return(
+        <div className="acv-rectangle-1">
+           <h3>Achievements</h3>
+        </div>
+    );
+}
+
+export default Achievements
