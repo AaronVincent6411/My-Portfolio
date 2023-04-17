271d847b92c2a80ea6085873d9b5db42011136f6
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-04-17 14:27:04 -0400

    added some contents to achievements

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -6,6 +6,11 @@ function Achievements() {
     return(
         <div className="acv-rectangle-1">
            <h3>Achievements</h3>
+           <p1>1. Secured 3rd Prize in Malarvadi Competition in Sub-District Level </p1>
+           <p1>2. Secured 1st Prize in School level DCL Quiz Competition</p1>
+           <p1>3. Secured 1st Prize in Best Out of Waste at school level</p1>
+           <p1>4. Me, Anju and Ashwin Pradeep won 1st in Eloquor Cluster 3 conducted by IEEE</p1>
+           <p1>5. Me and my friend Anju got 3rd Prize in Debate competition conducted by NSS NITC</p1>
         </div>
     );
 }
