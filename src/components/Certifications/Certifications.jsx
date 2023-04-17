da8757d422d343fd56724b3b4e98b5df74a0370c
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-04-17 15:05:35 -0400

    created Certifications

---
+++ src/components/Certifications/Certifications.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Certifications.css'
+
+function Certifications() {
+
+    return(
+        <div className="cer-rectangle-2">
+
+        </div>
+    );
+}
+
+export default Certifications
