2aa299f4e330ade74782608dd0cbc1e0744f24d6
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-04-19 14:05:57 -0400

    created certifications

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import './Certifications.css'
+import Header from "../Header/HeaderCER";
+import Navbar from "../Navbar/Navbar";
 
 function Certifications() {
+    const myStyle = {
+        background: 'linear-gradient(to right, #632636, #392A48)',
+        height: '100vh',
+        width: '100vw'
+      };
 
     return(
-        <div className="cer-rectangle-2">
-
+        <div style={myStyle}>
+            <div className="container">
+                <div className="rectangle">
+                    <Header/>
+                    <Navbar/>
+                    <div className="heading">Certifications</div>
+                    <div className="cer-rectangle-2">
+                    <p1>1. Python For Data Science from NPTEL</p1>
+                    <p1>2. Introduction to Tensorflow from Coursera</p1>
+                    <p1>3. Intro to Machine Learning</p1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
