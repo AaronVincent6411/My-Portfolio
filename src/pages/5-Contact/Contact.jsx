7e73050165cbc0c10f9564599c232554edbfad2b
AaronVincent6411 <aaronvincent6411@gmail.com>
2023-05-05 14:15:04 -0400

    added icons and navigation links

--- src/pages/5-Contact/Contact.jsx
+++ src/pages/5-Contact/Contact.jsx
@@ -3,6 +3,9 @@ import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/HeaderC";
 import './Contact.css'
 import ContactForm from "../../components/Form/Form";
+import { Link } from "react-router-dom";
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
+import {SiLinktree} from "react-icons/si";
 
 function Contact() {
     const myStyle = {
@@ -21,6 +24,35 @@ function Contact() {
                    <div className="contact-rectangle">
                     <h3>Connect me</h3>
                     <ContactForm/>
+                   </div>
+                   <div className="contact-rectangle-1">
+                    <img src="src/assets/contact-me.jpg"/>
+                   </div>
+                   <div className="contact-rectangle-2">
+                   <nav>
+                    <ul>
+                        <li>
+                        <Link to="https://github.com/aaronvincent6411" style={{ fontSize: "60px" }} target="_blank">
+                            <AiFillGithub/>
+                        </Link>
+                        </li>
+                       <li>
+                        <Link  to="https://www.linkedin.com/in/aaronvincent6411/" style={{ fontSize: "60px" }} target="_blank">
+                            <AiFillLinkedin/>
+                        </Link>
+                        </li>
+                        <li>
+                        <Link  to="https://www.instagram.com/aaronvincent6411/" style={{ fontSize: "60px" }} target="_blank">
+                            <AiFillInstagram/>
+                        </Link>
+                        </li>
+                        <li>
+                        <Link  to="https://linktr.ee/aaronvincent11" style={{ fontSize: "60px" }} target="_blank">
+                            <SiLinktree/>
+                        </Link>
+                        </li>
+                    </ul>
+                   </nav>
                    </div>
                 </div>
             </div>
