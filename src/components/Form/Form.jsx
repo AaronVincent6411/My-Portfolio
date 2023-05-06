1f22106fe13818f9891efe3610896ce36787b9a3
AaronVincent6411 <aaronvincent6411@gmail.com>
2023-05-06 17:39:48 -0400

    updated mailgun and tried adding gmail acception

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Form.css'
 
+
 function ContactForm() {
+// const mailgun = new Mailgun(formData);
+// const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || '45c194c167b103a0d53b03236714a8c5-181449aa-1b8f7c44'});
+
+// mg.messages.create('sandbox-123.mailgun.org', {
+// 	from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+// 	to: ["aaronvincent6411@gmail.com"],
+// 	subject: "Hello",
+// 	text: "Testing some Mailgun awesomeness!",
+// 	html: "<h1>Testing some Mailgun awesomeness!</h1>"
+// })
+// .then(msg => console.log(msg)) // logs response data
+// .catch(err => console.log(err)); // logs any error
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -11,18 +24,18 @@ function ContactForm() {
     event.preventDefault();
 
     const data = {
-      from: `${name} <${email}>`,
+      from: 'aaronvincent6411@gmail.com',
       to: 'aaronvincent6411@gmail.com',
       subject: 'New message from your website',
-      text: message,
+      text: `${name} ${message}`,
     };
 
     axios({
       method: 'post',
-      url: 'https://api.mailgun.net/v3/https://app.mailgun.com/app/sending/domains/sandboxd31f4e7da4374123bbd2f71e3afbb478.mailgun.org/messages',
+      url: 'https://api.mailgun.net/v3/sandboxd31f4e7da4374123bbd2f71e3afbb478.mailgun.org/messages',
       auth: {
         username: 'api',
-        password: '181449aa-1b8f7c44',
+        password: '45c194c167b103a0d53b03236714a8c5-181449aa-1b8f7c44',
       },
       data,
     })
@@ -38,6 +51,45 @@ function ContactForm() {
         alert('Sorry, there was an error sending your message. Please try again later.');
       });
   };
+
+  // const mailgun = require("mailgun-js");
+  // const DOMAIN = "YOUR_DOMAIN_NAME";
+  // const mg = mailgun({ apiKey: 'process.env.MAILGUN_API_KEY', domain: DOMAIN });
+  
+  // function submitForm(formData) {
+  //   const data = {
+  //     from: "Connect Me <connectme@YOUR_DOMAIN_NAME>",
+  //     to: "YOUR_EMAIL_ADDRESS",
+  //     subject: "New Connect Me Submission",
+  //     html: `<p>Name: ${formData.name}</p><p>Email: ${formData.email}</p><p>Message: ${formData.message}</p>`
+  //   };
+  
+  //   mg.messages().send(data, function(error, body) {
+  //     console.log(body);
+  //   });
+  // }
+
+//   const API_KEY = '45c194c167b103a0d53b03236714a8c5-181449aa-1b8f7c44';
+// const DOMAIN = 'https://app.mailgun.com/app/sending/domains/sandboxd31f4e7da4374123bbd2f71e3afbb478.mailgun.org';
+
+// const mailgun = new Mailgun(formData);
+// const client = mailgun.client({username: 'api', key: API_KEY});
+
+// const messageData = {
+//   from: 'Excited User <me@samples.mailgun.org>',
+//   to: 'aaronvincent6411@gmail.com',
+//   subject: 'Hello',
+//   text: 'New Message'
+// };
+
+// client.messages.create(DOMAIN, messageData)
+//  .then((res) => {
+//    console.log(res);
+//  })
+//  .catch((err) => {
+//    console.error(err);
+//  });
+  
 
   return (
     <form className='contact-form' onSubmit={handleSubmit}>
@@ -55,6 +107,16 @@ function ContactForm() {
       </label>
       <button type="submit">Send</button>
     </form>
+//     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdEAFO7Twc5NjvNCYezG2HYPGU6g3abXV2sIjh0T7pZ_AZNQ/viewform?embedded=true" 
+//   width="500" 
+//   height="600" 
+//   frameborder="0" 
+//   marginheight="0" 
+//   marginwidth="0">
+//   Loading…
+// </iframe>
+
+
   );
 }
 
