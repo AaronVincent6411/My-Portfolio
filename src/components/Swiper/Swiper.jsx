0845f4d7a4ca3be81da0e86d9ad71baabe9e891d
AaronVincent6411 <aaronvincent6411@gmail.com>
2023-05-19 13:43:31 -0400

    added Supermarket link

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -13,7 +13,7 @@ function Slider() {
       id: 1,
       title: 'Supermarket',
       description: 'This is a project done by me and my friend Sooraj when we were studying in class 12.With the help of this project written in C++ language we can write and read products available in the supermarket and also enter customer product details and return the cost and total amount of products of the respective customer',
-      github: '',
+      github: 'https://github.com/AaronVincent6411/Supermarket',
       image: 'https://live.staticflickr.com/65535/52803441182_8671655a7c_n.jpg'
     },
     {
