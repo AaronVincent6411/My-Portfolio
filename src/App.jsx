b846262b70fedbdf5b6cb1ae6f05692f9ac00dd0
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-06-14 23:24:20 +0530

    adjusted screen size

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Overview from "./pages/1-Overview/Overview";
 
 function App() {
-  const myStyle = {
-    background: 'linear-gradient(to right, #632636, #392A48)',
-    height: '100vh',
-    width: '100vw'
-  };
+  // const myStyle = {
+  //   background: 'linear-gradient(to right, #632636, #392A48)',
+  //   height: '100vh',
+  //   width: '100vw'
+  // };
 
-  const isMobile = window.innerWidth <= 1400;
+  const isMobile = window.innerWidth <= 1025;
 
   return (
-    <div style={myStyle}>
+    <div>
       {isMobile ? (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           <h2>Sorry friends i havn't made website responsive 😅 . Please use a laptop or desktop (15.6 inch size) to view this website 😁.</h2>
