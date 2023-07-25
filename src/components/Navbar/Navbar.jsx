56afb4c7b06da7ba52f97ae38981c8188d955c9a
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-07-25 12:58:53 +0530

    created navbar for mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { FaClock } from 'react-icons/fa';
 import { AiOutlineMail, AiFillProject } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
-import projectIcon from '../../assets/Projects-icon.png'
-
 // import { IconName } from "react-icons/md";
-import { GrProjects } from "react-icons/gr";
 import { GiAchievement, GiSkills } from "react-icons/gi"
 
 function Navbar() {
 
-  const Mobile = window.innerWidth < 426;
+  const Mobileview = window.innerWidth < 500;
+
+  const [isSidepanelOpen, setSidepanelOpen] = useState(false);
+
+  const toggleSidepanel = () => {
+    setSidepanelOpen(!isSidepanelOpen);
+  };
 
 return(
   <div className="inner-rectangle">
-    {Mobile ? (
+    {Mobileview ? (
       // Mobile View
-        <div className="menu-toogle">
+      <div>
+      <button className="openbtn" onClick={toggleSidepanel}>
+        &#9776;
+      </button>
+      <div className={`sidepanel ${isSidepanelOpen ? "open" : ""}`}>
+        <button className="closebtn" onClick={toggleSidepanel}>
+          &times;
+        </button>
         <nav>
           <ul>
-          <button>
             <li>
-              <Link to="/">
-                <FontAwesomeIcon icon={faHome}/>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; Overview
+              <Link to="/" onClick={toggleSidepanel}>
+                <FontAwesomeIcon icon={faHome} />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Overview
               </Link>
             </li>
             <li>
-              <Link to="/about">
-                <FontAwesomeIcon icon={faInfoCircle}/>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; About
+              <Link to="/about" onClick={toggleSidepanel}>
+                <FontAwesomeIcon icon={faInfoCircle} />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; About
               </Link>
             </li>
             <li>
-              <Link to="/projects"><AiFillProject />&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Projects</Link>
+              <Link to="/projects" onClick={toggleSidepanel}>
+                <AiFillProject />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Projects
+              </Link>
             </li>
             <li>
-              <Link to="/competencies"><GiSkills/><GiAchievement />&nbsp;&nbsp;Competencies</Link>
+              <Link to="/competencies" onClick={toggleSidepanel}>
+                <GiSkills />
+                <GiAchievement />&nbsp;&nbsp;Competencies
+              </Link>
             </li>
             <li>
-              <Link to="/contact"><AiOutlineMail/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Contact</Link>
+              <Link to="/contact" onClick={toggleSidepanel}>
+                <AiOutlineMail />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Contact
+              </Link>
             </li>
-            </button>
           </ul>
         </nav>
-       </div>
+      </div>
+    </div>
       ) : (
         // Laptop and Desktop View
     <nav>
