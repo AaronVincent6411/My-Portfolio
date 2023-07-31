43216ef3b7b41ac0d71f02eb1bf63165b9b76a3d
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-07-31 22:03:12 +0530

    adjusted position and marquee content

--- src/components/Header/HeaderCO.jsx
+++ src/components/Header/HeaderCO.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import './Header.css';
 import { Link } from "react-router-dom";
 
+const isMobile = window.innerWidth <= 425;
+
 function Header() {
 
     return(
         <div>
         <div className='inner-rectangle-8'>
+        {isMobile ? (
+            //Mobile View
+            <div className="marquee">
+                <ul className="marquee__content ">
+                   <li>&nbsp;Competencies&nbsp;</li>
+                   <li>&nbsp;Competencies&nbsp;</li>
+                   <li>&nbsp;Competencies&nbsp;</li>
+                </ul>
+                <ul className="marquee__content">
+                   <li>&nbsp;Competencies&nbsp;</li>
+                   <li>&nbsp;Competencies&nbsp;</li>
+                </ul>
+                </div>
+                ) : (
+                    //Laptop View
             <div className="marquee">
                 <ul className="marquee__content ">
                     <li>&nbsp;Competencies&nbsp;</li>
@@ -27,6 +44,7 @@ function Header() {
                     <li>&nbsp;Competencies&nbsp;</li>
                 </ul>
                 </div>
+                )}
             </div>
             <Link to='/'>
                 <img src="https://live.staticflickr.com/65535/52783270220_87fdc16456_b.jpg" className='lionex-png'/>
