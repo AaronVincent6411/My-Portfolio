9b89da7740bfa3b32becdafca5ac6636f0804a18
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-12-20 00:05:38 +0530

    removed unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,7 @@ function App() {
   const isMobile = window.innerWidth < 200;
 
   return (
-    <div>
-      {isMobile ? (
-        <div style={{ display: 'flex', color:'white', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-          <h2>Sorry friends i havn't made website responsive 😅 . Please use a laptop or desktop (width 1920 px or width 1440 px) to view this website 😁.</h2>
-        </div>
-      ) : (
-        <div>
-          <Overview />
-        </div>
-      )}
-    </div>
+    <Overview />
   );
 }
 
