6a8420f7d2712492aaa86a4d2e2016b4b2401f0c
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-11 23:45:43 +0530

    modified app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import Overview from "./pages/1-Overview/Overview";
+import Home from "./pages/Home/Home";
+import './App.css'
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Projects from "./pages/Projects/Projects";
+import Competencies from "./pages/Competencies/Competencies";
+
 
 function App() {
-  // const myStyle = {
-  //   background: 'linear-gradient(to right, #632636, #392A48)',
-  //   height: '100vh',
-  //   width: '100vw'
-  // };
-
-  const isMobile = window.innerWidth < 200;
 
   return (
-    <Overview />
-  );
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/competencies" element={<Competencies />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route
+            path="*"
+            element={<PageNotFound />}
+        />
+      </Routes>
+    </Router>
+    );
 }
 
 export default App;
