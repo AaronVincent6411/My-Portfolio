fa87342a6d50c1e72011ff491ae01ae10627038c
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-14 23:47:41 +0530

    added tailwind and updated some packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,17 @@ module.exports = {
   theme: {
     extend: {fontSize: {
       '3xl': ['32px'],
-    }},
-    
+      '4xl': ['36px'],
+    },
+    spacing: {
+      '84': '21rem',
+      '95': '25rem',
+      '100':'30rem',
+      '115':'45rem',
+      '120':'50rem',
+      '130':'60rem'
+    }
+  },
   },
   plugins: [],
 }
